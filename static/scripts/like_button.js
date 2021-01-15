492ac5915cfe01add21a3750c7427f26d6a1d633
lsown <aureias@gmail.com>
2021-01-15 15:14:36 -0800

    updated

--- static/scripts/like_button.js
+++ static/scripts/like_button.js
@@ -52,7 +52,7 @@ class NewLikeButton extends React.Component {
     
     render() {
       return (
-        ("input", {type:"text", id:"name", name:"name" })
+        ("input", {type:"text", id:"name", name:"name", size:"10", })
       );
     }
   }
