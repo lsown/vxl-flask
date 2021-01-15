92e1aa36cb80275ac5455e6d6025266a90b242f7
lsown <aureias@gmail.com>
2021-01-15 13:26:16 -0800

    updated

--- static/scripts/like_button.js
+++ static/scripts/like_button.js
@@ -34,17 +34,45 @@ class NewLikeButton extends React.Component {
             { onClick: () => this.setState({ liked: false})},
         'Unlike'
         );
+      } else {
+        return e(
+          'button',
+          { onClick: () => this.setState({ liked: true }) },
+          'Like'
+        );
       }
-  
-      return e(
-        'button',
-        { onClick: () => this.setState({ liked: true }) },
-        'Like'
-      );
     }
   }
 
+  class voxaTextField extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { field: "" };
+    }
 
+    setFieldValue = event => {
+      this.setState({
+        field: event.target.value
+      });
+    };
+
+    render() {
+      <p>Hello</p>
+    }
+  }
+
+  function Example() {
+    // Declare a new state variable, which we'll call "count"  const 
+    [count, setCount] = useState(0);
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
+    );
+  }
 
 // Find all DOM containers, and render Like buttons into them.
 document.querySelectorAll('.like_button_container')
@@ -68,4 +96,4 @@ document.querySelectorAll('.like_button_container')
   });
 
   
-ReactDOM.render('hello', document.querySelector('.counter_button'));
+ReactDOM.render(voxaTextField, document.querySelector('.counter_button'));
