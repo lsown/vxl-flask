ec1ccd7c9aa0b53ccb20d4b7af665298c75d34ba
lsown <aureias@gmail.com>
2021-01-15 17:50:09 -0800

    updated

--- static/scripts/like_button.js
+++ static/scripts/like_button.js
@@ -105,13 +105,7 @@ document.querySelectorAll('.like_button_container')
     );
   });
   
-ReactDOM.render(
-  e(inputLabel, null, e(inputText, null)), 
-  document.querySelector('.panel4')
-)
+ReactDOM.render(e(inputLabel), document.querySelector('.panel4'));
 
 
-ReactDOM.render(
-  e(inputText), 
-  document.querySelector('.panel3')
-)
+ReactDOM.render(e(inputText), document.querySelector('.panel3'));
