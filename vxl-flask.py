efd5e76031baa6592021945269a4fd6dedef5cc8
lsown <aureias@gmail.com>
2021-02-17 23:49:54 -0800

    updated

--- vxl-flask.py
+++ vxl-flask.py
@@ -48,7 +48,7 @@ def get_readbacks(polltime = 2):
             rb_dict[i][1] = random.randrange(0, 10)
         socketio.emit('readback_msg', 
             {'data' : rb_dict}) #try adding it to namespace='/readbacks' later
-        print(f'Representative readback from rb0: {rb_dict["rowA"][0]}:{rb_dict["rowA"][1]}:{rb_dict["rowA"][2]}')  #for validation purposes
+        print(f'Readback from rb0: {rb_dict["rowA"][0]}: {rb_dict["rowA"][1]} {rb_dict["rowA"][2]}')  #for validation purposes
 
 def backMonitor():
     global thread
